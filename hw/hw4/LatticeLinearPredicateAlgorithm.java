94ea17d558b5a60cdc625209b6828a6eaed26883
DarkQuark26 <darkspark26@gmail.com>
2025-10-19 00:05:11 -0500

    LLPRunner initial progress

--- hw/hw4/LatticeLinearPredicateAlgorithm.java
+++ hw/hw4/LatticeLinearPredicateAlgorithm.java
@@ -2,6 +2,11 @@ package hw.hw4;
 
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.function.*;
+
 public class LatticeLinearPredicateAlgorithm {
 
 class GlobalState {
@@ -16,10 +21,21 @@ class GlobalState {
         this.state = new AtomicIntegerArray(n);
     }
 
+    /**
+     * Return G[j]
+     * 
+     * @param index j
+     * @return G[j]
+     */
     public int get(int index) {
         return state.get(index);
     }
 
+    /**
+     * 
+     * @param index
+     * @param value
+     */
     public void set(int index, int value) {
         state.set(index, value);
     }
@@ -55,21 +71,63 @@ public interface LLPAlgorithm {
     // Add ensure for alternative to orbidden and advance?
     void init(ThreadContext ctx);
 
+    /**
+     * Check if state G[j] is forbidden
+     * 
+     * @param ctx ThreadContext object that tracks index in G
+     * @return True if state is forbidden
+     */
     boolean forbidden(ThreadContext ctx);
 
+    /**
+     * Advance state G[j]
+     * 
+     * @param ctx
+     * @return New state for G[j]
+     */
     int advance(ThreadContext ctx);
 
+    // Could use an auxiliary array to track if all threads are
+    // not advancing, or just wait and check if G hasn't been
+    // modified in a while?
     default boolean hasConverged(GlobalState G) {
-        return false;
+        return false; 
     }
     
 }
 
 class LLPRunner {
-    private final int n;
-    private final GlobalState G;
+    private final int n; // Number of elements in global state
+    private final GlobalState G; //
     private final LLPAlgorithm algorithm;
-    private final 
+    
+    private final int numPlatformThreads; // Number of platform threads to use
+    
+    
+    public LLPRunner(int n, LLPAlgorithm algorithm) {
+        this(n, algorithm, Runtime.getRuntime().availableProcessors());
+    }
+
+
+    public void start() {
+        // Initalize logical threads (to be used by platofrm threads)
+        for (int j = 0; j < n; j++) {
+            final int threadID = j;
+            ThreadContext ctx = new ThreadContext(threadID, G);
+            algorithm.init(ctx);
+            initLatch.countDown();
+        }
+
+        // Populate work queue
+        for (int j = 0; j < n; j++) {
+            workQueue.offer(j)
+        }
+
+        // Start platform threads
+        for (int p = 0; p < numPlatformThreads; p++) {
+            runner.submit(this::platformThreadLoop)
+        }
+    }
 
 }
 
